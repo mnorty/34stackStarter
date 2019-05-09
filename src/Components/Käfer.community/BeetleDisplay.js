9a9539812175052c176116dccd748c168d414cbc
mnorty <mnorty@gmail.com>
2019-05-09 12:16:51 -0600

    tweaking beetle display

--- src/Components/Käfer.community/BeetleDisplay.js
+++ src/Components/Käfer.community/BeetleDisplay.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './Beetle.css';
 
 class PokemonDisplay extends Component {
-    handleDeletePokemon = () => {
+    handleDeleteBeetle = () => {
         axios.delete(`/api/pokemon/${this.props.pokemon.id}`)
         .then(res => {
             this.props.deletePokemon(res.data)
@@ -13,9 +13,10 @@ class PokemonDisplay extends Component {
     render(){
         return (
         <div>
-            <h4 className='pokemon-name'>{this.props.pokemon.name}</h4>
-            <img src={this.props.pokemon.image} alt='cool pokemon' className='pokemon-pictures'/>
-            <button onClick={this.handleDeletePokemon}>Delete</button>
+            <h4 className='beetle-name'>{this.props.pokemon.name}</h4>
+            <img src={this.props.pokemon.image} alt='"Groovy Bug man"' className='beetle-pictures'/>
+            <button onClick={this.handleDeleteBeetle}>Delete</button>
+            <button onClick={this.handleDeleteBeetle}>Edit</button>     
         </div>
     )
     }
