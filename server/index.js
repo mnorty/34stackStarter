435799a7e5dddda48ae51dab1897b5d58e46eeb3
mnorty <mnorty@gmail.com>
2019-05-13 11:48:12 -0600

    index.js notes done

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors')
 
 app.use(cors());
+// cors helps ports not matching up
 app.use(json());
 
 
@@ -12,6 +13,9 @@ app.get('/api/beetles', ctrl.getBeetle);
 app.delete('/api/beetle/:id', ctrl.deleteBeetle);
 app.post('/api/createbeetle' , ctrl.createBeetle)
 app.put('/api/updatebeetle/:id' , ctrl.changeBeetle)
+//these are what the client initially hits, then depending on the type of request it launches a different function. 
 
 const server_port = 5056;
+//what port we are running at 
 app.listen(server_port, () => console.log(`Aircooled Magic at port: ${server_port}`))
+// just a message letting us know the server is running properly
